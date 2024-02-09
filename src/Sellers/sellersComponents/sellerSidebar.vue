5061d01f6a81bc9b39de9be5073597f74b22930b
Your Name <deudajm@gmail.com>
2024-02-09 11:50:46 +0800

    new updates

--- src/Sellers/sellersComponents/sellerSidebar.vue
+++ src/Sellers/sellersComponents/sellerSidebar.vue
@@ -1,103 +1,105 @@
 <template>
-  <div class="bg-gray-100/10 shadow h-screen w-60">
-    <div class="py-3 px-3 font-semibold text-gray-700"><h1>Logo</h1></div>
-    <div class="px-2">
-      <hr />
-      <h1 class="font-semibold text-gray-500 py-3">Main Menu</h1>
-      <hr />
-      <div>
-        <RouterLink to="/seller_products">
-          <div
-            :class="{
-              ' text-slate-700 bg-sky-100/25 shadow font-medium ':
-                $route.name === 'seller_products',
-              'text-slate-500 bg-sky-100/10 font-normal':
-                $route.name !== 'seller_products',
-              ' font-semibold text-base': true,
-            }"
-            class="px-2 py-1 my-2 flex gap-2 justify-start rounded-md items-center"
-          >
-            <Icon icon="akar-icons:home" class="text-xl" />
-            Dashboard
-          </div>
-        </RouterLink>
-        <RouterLink to="/">
-          <div
-            :class="{
-              ' text-slate-700 bg-sky-100/25 shadow font-medium':
-                $route.name === '',
-              'text-slate-500 bg-sky-100/10 font-normal': $route.name !== '',
-              ' font-semibold text-base': true,
-            }"
-            class="px-2 py-1 my-2 flex gap-2 justify-start rounded-md items-center"
-          >
-            <Icon icon="bi:cart" class="text-xl" />
-            Order Management
-          </div>
-        </RouterLink>
-        <RouterLink to="/">
-          <div
-            :class="{
-              ' text-slate-700 bg-sky-100/25 shadow font-medium':
-                $route.name === '',
-              'text-slate-500 bg-sky-100/10 font-normal': $route.name !== '',
-              ' font-semibold text-base': true,
-            }"
-            class="px-2 py-1 my-2 flex gap-2 justify-start rounded-md items-center"
-          >
-            <Icon icon="bi:people" class="text-xl" />
-            Customers
-          </div>
-        </RouterLink>
-        <RouterLink to="/">
-          <div
-            :class="{
-              ' text-slate-700 bg-sky-100/25 shadow font-medium':
-                $route.name === '',
-              'text-slate-500 bg-sky-100/10 font-normal': $route.name !== '',
-              ' font-semibold text-base': true,
-            }"
-            class="px-2 py-1 my-2 flex gap-2 justify-start rounded-md items-center"
-          >
-            <Icon icon="bitcoin-icons:transactions-filled" class="text-xl" />
-            Transactions
-          </div>
-        </RouterLink>
+  <div class="bg-gray-100/10 shadow min-h-screen w-60">
+    <div class="h-full">
+      <div class="py-3 px-3 font-semibold text-gray-700"><h1>Logo</h1></div>
+      <div class="px-2">
+        <hr />
+        <h1 class="font-semibold text-gray-500 py-3">Main Menu</h1>
+        <hr />
         <div>
-          <hr />
-          <h1 class="font-semibold text-gray-500 py-3">Products</h1>
-          <hr />
-        </div>
-        <RouterLink to="/seller_Add_products">
-          <div
-            :class="{
-              ' text-slate-700 bg-sky-100/25 shadow font-medium':
-                $route.name === 'seller_Add_products',
-              'text-slate-500 bg-sky-100/10 font-normal':
-                $route.name !== 'seller_Add_products',
-              ' font-semibold text-base': true,
-            }"
-            class="px-2 py-1 my-2 flex gap-2 justify-start rounded-md items-center"
-          >
-            <Icon icon="gala:add" class="text-xl" />
+          <RouterLink to="/seller_products">
+            <div
+              :class="{
+                ' text-slate-700 bg-sky-100/25 shadow font-medium ':
+                  $route.name === 'seller_products',
+                'text-slate-500 bg-sky-100/10 font-normal':
+                  $route.name !== 'seller_products',
+                ' font-semibold text-base': true,
+              }"
+              class="px-2 py-1 my-2 flex gap-2 justify-start rounded-md items-center"
+            >
+              <Icon icon="akar-icons:home" class="text-xl" />
+              Dashboard
+            </div>
+          </RouterLink>
+          <RouterLink to="/">
+            <div
+              :class="{
+                ' text-slate-700 bg-sky-100/25 shadow font-medium':
+                  $route.name === '',
+                'text-slate-500 bg-sky-100/10 font-normal': $route.name !== '',
+                ' font-semibold text-base': true,
+              }"
+              class="px-2 py-1 my-2 flex gap-2 justify-start rounded-md items-center"
+            >
+              <Icon icon="bi:cart" class="text-xl" />
+              Order Management
+            </div>
+          </RouterLink>
+          <RouterLink to="/">
+            <div
+              :class="{
+                ' text-slate-700 bg-sky-100/25 shadow font-medium':
+                  $route.name === '',
+                'text-slate-500 bg-sky-100/10 font-normal': $route.name !== '',
+                ' font-semibold text-base': true,
+              }"
+              class="px-2 py-1 my-2 flex gap-2 justify-start rounded-md items-center"
+            >
+              <Icon icon="bi:people" class="text-xl" />
+              Customers
+            </div>
+          </RouterLink>
+          <RouterLink to="/">
+            <div
+              :class="{
+                ' text-slate-700 bg-sky-100/25 shadow font-medium':
+                  $route.name === '',
+                'text-slate-500 bg-sky-100/10 font-normal': $route.name !== '',
+                ' font-semibold text-base': true,
+              }"
+              class="px-2 py-1 my-2 flex gap-2 justify-start rounded-md items-center"
+            >
+              <Icon icon="bitcoin-icons:transactions-filled" class="text-xl" />
+              Transactions
+            </div>
+          </RouterLink>
+          <div>
+            <hr />
+            <h1 class="font-semibold text-gray-500 py-3">Products</h1>
+            <hr />
+          </div>
+          <RouterLink to="/seller_Add_products">
+            <div
+              :class="{
+                ' text-slate-700 bg-sky-100/25 shadow font-medium':
+                  $route.name === 'seller_Add_products',
+                'text-slate-500 bg-sky-100/10 font-normal':
+                  $route.name !== 'seller_Add_products',
+                ' font-semibold text-base': true,
+              }"
+              class="px-2 py-1 my-2 flex gap-2 justify-start rounded-md items-center"
+            >
+              <Icon icon="gala:add" class="text-xl" />
 
-            Add Products
-          </div>
-        </RouterLink>
-        <RouterLink to="/">
-          <div
-            :class="{
-              ' text-slate-700 bg-sky-100/25 shadow font-medium':
-                $route.name === '',
-              'text-slate-500 bg-sky-100/10 font-normal': $route.name !== '',
-              ' font-semibold text-base': true,
-            }"
-            class="px-2 py-1 my-2 flex gap-2 justify-start rounded-md items-center"
-          >
-            <Icon icon="cil:list" class="text-xl" />
-            Product List
-          </div>
-        </RouterLink>
+              Add Products
+            </div>
+          </RouterLink>
+          <RouterLink to="/">
+            <div
+              :class="{
+                ' text-slate-700 bg-sky-100/25 shadow font-medium':
+                  $route.name === '',
+                'text-slate-500 bg-sky-100/10 font-normal': $route.name !== '',
+                ' font-semibold text-base': true,
+              }"
+              class="px-2 py-1 my-2 flex gap-2 justify-start rounded-md items-center"
+            >
+              <Icon icon="cil:list" class="text-xl" />
+              Product List
+            </div>
+          </RouterLink>
+        </div>
       </div>
     </div>
   </div>
--- src/Sellers/sellersViews/sellerProducts.vue
+++ src/Sellers/sellersViews/sellerProducts.vue
@@ -1,3 +1,76 @@
 <template>
-  <h1>Hahhahahahahha</h1>
+  <div class="mx-4">
+    <div class="py-3 px-2 font-bold text-2xl text-slate-700">
+      <h1>Dashboard</h1>
+    </div>
+    <div class="sm:flex gap-5">
+      <div class="bg-slate-400/10 rounded-md h-32 w-full text-slate-700 shadow">
+        <div class="p-2">
+          <h1 class="text-base font-semibold">Total Sales & Cost</h1>
+          <p class="text-xs">Last 7 Days</p>
+        </div>
+        <div class="p-2">
+          <h1 class="text-3xl font-extrabold">$ 5, 000, 000.00</h1>
+        </div>
+      </div>
+      <div class="bg-slate-400/10 rounded-md h-32 w-96 text-slate-700 shadow">
+        <div class="p-2">
+          <h1 class="text-base font-semibold">Sessions</h1>
+          <p class="text-xs">Last 7 Days</p>
+        </div>
+      </div>
+    </div>
+    <div class="sm:flex gap-5 my-2">
+      <div
+        class="bg-slate-400/10 my-2 rounded-md h-32 w-52 text-slate-700 shadow"
+      >
+        <div class="p-2">
+          <h1 class="text-base font-semibold">Total Sales & Cost</h1>
+          <p class="text-xs">Last 7 Days</p>
+        </div>
+      </div>
+      <div
+        class="bg-slate-400/10 my-2 rounded-md h-32 w-96 text-slate-700 shadow"
+      >
+        <div class="p-2">
+          <h1 class="text-base font-semibold">Sessions</h1>
+          <p class="text-xs">Last 7 Days</p>
+        </div>
+      </div>
+      <div
+        class="bg-slate-400/10 my-2 rounded-md h-32 w-52 text-slate-700 shadow"
+      >
+        <div class="p-2">
+          <h1 class="text-base font-semibold">Total Sales & Cost</h1>
+          <p class="text-xs">Last 7 Days</p>
+        </div>
+      </div>
+      <div
+        class="bg-slate-400/10 my-2 rounded-md h-32 w-52 text-slate-700 shadow"
+      >
+        <div class="p-2">
+          <h1 class="text-base font-semibold">Total Sales & Cost</h1>
+          <p class="text-xs">Last 7 Days</p>
+        </div>
+      </div>
+    </div>
+    <div class="sm:flex gap-5 my-2">
+      <div
+        class="bg-slate-400/10 my-2 rounded-md h-72 w-full text-slate-700 shadow"
+      >
+        <div class="p-2">
+          <h1 class="text-base font-semibold">Total Sales & Cost</h1>
+          <p class="text-xs">Last 7 Days</p>
+        </div>
+      </div>
+      <div
+        class="bg-slate-400/10 my-2 rounded-md h-72 w-52 text-slate-700 shadow"
+      >
+        <div class="p-2">
+          <h1 class="text-base font-semibold">Total Sales & Cost</h1>
+          <p class="text-xs">Last 7 Days</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
