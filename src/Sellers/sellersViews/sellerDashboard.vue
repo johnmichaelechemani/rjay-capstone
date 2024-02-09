a75717edcc1f3d7bb7cd2549b0f9a671a1dc0007
Your Name <deudajm@gmail.com>
2024-02-09 18:42:28 +0800

    add close and show btn to sidebar

--- src/Sellers/sellersViews/sellerDashboard.vue
+++ src/Sellers/sellersViews/sellerDashboard.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="flex">
-    <sellerSidebar />
+    <div v-if="isShow">
+      <sellerSidebar />
+    </div>
+    <div class="relative">
+      <div
+        @click="showSidebarMenu"
+        class="absolute left-3 top-3 bg-slate-500/10 p-2 rounded-full shadow"
+      >
+        <Icon icon="oui:menu-left" class="text-lg" />
+      </div>
+    </div>
     <div class="absolute right-5 top-2">
       <div class="p-1 bg-slate-500/10 shadow rounded-full relative">
         <Icon
@@ -18,10 +28,21 @@
 <script>
 import sellerSidebar from "../sellersComponents/sellerSidebar.vue";
 import { Icon } from "@iconify/vue";
+import { ref } from "vue";
 export default {
   components: {
     sellerSidebar,
     Icon,
+  },
+  setup() {
+    const isShow = ref(true);
+    const showSidebarMenu = () => {
+      isShow.value = !isShow.value;
+    };
+    return {
+      showSidebarMenu,
+      isShow,
+    };
   },
 };
 </script>
