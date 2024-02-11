68c79a632505433037f7e7f571566d45648a5987
Your Name <deudajm@gmail.com>
2024-02-11 09:30:17 +0800

    add product table list

--- src/Sellers/sellersViews/sellerProductList.vue
+++ src/Sellers/sellersViews/sellerProductList.vue
@@ -3,5 +3,36 @@
     <div class="py-3 px-10 font-bold text-2xl text-slate-700">
       <h1>Product Lists</h1>
     </div>
+    <div class="my-5">
+      <div class="relative overflow-x-auto shadow rounded-md">
+        <table
+          class="w-full text-sm text-left rtl:text-right text-gray-900 rounded-md"
+        >
+          <thead
+            class="text-xs text-slate-800 bg-slate-100/20 uppercase rounded-md"
+          >
+            <tr class="bg-gray-100/10 border-b border-gray-600/50">
+              <th scope="col" class="px-6 py-3">Product Name</th>
+              <th scope="col" class="px-6 py-3">Created</th>
+              <th scope="col" class="px-6 py-3">Customer</th>
+              <th scope="col" class="px-6 py-3">Price</th>
+            </tr>
+          </thead>
+          <tbody class="">
+            <tr class="bg-gray-100/10 border-b border-gray-600/50">
+              <th
+                scope="row"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+              >
+                Intel Core i9-11900KF
+              </th>
+              <td class="px-6 py-4">January 1, 2022</td>
+              <td class="px-6 py-4">John Doe</td>
+              <td class="px-6 py-4">$2999</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
